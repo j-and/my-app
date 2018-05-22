1f9d86dcf55fd9aa99977caeb981c91e5c8413db
yastrouskaya <yastrouskaya@exadel.com>
2018-05-22 10:53:14 +0300

    Button Save is added

--- src/Register.js
+++ src/Register.js
@@ -11,7 +11,7 @@ class Register extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {time: 0, name: 0};
+        this.state = {time: 'Time', value: ''};
         this.clear = this.clear.bind(this);
         this.enterName = this.enterName.bind(this);
         this.save = this.save.bind(this);
@@ -22,43 +22,43 @@ class Register extends Component {
     clear(e) {
         e.preventDefault();
         alert('Delete registerObject');
-        this.setState({value: 0});
+        this.setState({value: ''});
+        this.setState({time: 'Time'});
     }
 
     enterName(event) {
-        var newRegisterObject = new RegisterObject('', '', '', '');
-        newRegisterObject.setFullName(event.target.value);
+        this.setState({value:event.target.value});
     }
 
-    save() {
-
-    }
-    
-    selectTime(eventKey) {
-        this.state = {time: eventKey};
+    save(event) {
         var newRegisterObject = new RegisterObject('', '', '', '');
+        newRegisterObject.setFullName(this.state.value);
         newRegisterObject.setTime(this.state.time);
+    }
+
+    selectTime(eventKey) {
+        this.setState({time: eventKey});
     }
 
     render() {
 
         return (
             <div>
-                <DropdownButton title="Time" id="1" onSelect={this.selectTime}>
-                    <MenuItem eventKey="8">08.00</MenuItem>
-                    <MenuItem eventKey="9">09.00</MenuItem>
-                    <MenuItem eventKey="10">10.00</MenuItem>
-                    <MenuItem eventKey="11">11.00</MenuItem>
-                    <MenuItem eventKey="12">12.00</MenuItem>
-                    <MenuItem eventKey="13">13.00</MenuItem>
-                    <MenuItem eventKey="14">14.00</MenuItem>
-                    <MenuItem eventKey="15">15.00</MenuItem>
-                    <MenuItem eventKey="16">16.00</MenuItem>
-                    <MenuItem eventKey="17">17.00</MenuItem>
+                <DropdownButton title={this.state.time} id="1" onSelect={this.selectTime}>
+                    <MenuItem eventKey="08.00">08.00</MenuItem>
+                    <MenuItem eventKey="09.00">09.00</MenuItem>
+                    <MenuItem eventKey="10.00">10.00</MenuItem>
+                    <MenuItem eventKey="11.00">11.00</MenuItem>
+                    <MenuItem eventKey="12.00">12.00</MenuItem>
+                    <MenuItem eventKey="13.00">13.00</MenuItem>
+                    <MenuItem eventKey="14.00">14.00</MenuItem>
+                    <MenuItem eventKey="15.00">15.00</MenuItem>
+                    <MenuItem eventKey="16.00">16.00</MenuItem>
+                    <MenuItem eventKey="17.00">17.00</MenuItem>
                     <MenuItem divider/>
                     <MenuItem eventKey="4">Add</MenuItem>
                 </DropdownButton>
-                <input className="input-name" placeholder="Name" value={this.state.value} onChange={this.enterName}/>
+                <input class="input-name" placeholder="Name" value={this.state.value} onChange={this.enterName}/>
                 <Button bsSize="xsmall" bsStyle="success" className="btn-close" onClick={this.save}> <Glyphicon
                     glyph="ok"/></Button>
                 <Button bsSize="xsmall" bsStyle="danger" className="btn-close" onClick={this.clear}> <Glyphicon
